0757c9c531afe57eadde86198efacb3a3ee1a939
Brendon <brendon.biagi@gmail.com>
2020-09-25 17:11:34 -0400

    Fix syntax error

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,5 +1,5 @@
 class Quest < ApplicationRecord
-    validates :quest_name, :category_id, :details, :creator_id presence: true
+    validates :quest_name, :category_id, :details, :creator_id, presence: true
     validates :adventurer_id, :start_time, allow_nil: true
     
     belongs_to :user
