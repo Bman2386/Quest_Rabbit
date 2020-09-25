79c030dc990270d6d3c173187f10a70e6b321e6b
Brendon <brendon.biagi@gmail.com>
2020-09-25 17:27:07 -0400

    Fix another bug

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,6 +1,6 @@
 class Quest < ApplicationRecord
     validates :quest_name, :category_id, :details, :creator_id, presence: true
-    validates :adventurer_id, :start_time, allow_nil: true
+    # validates :adventurer_id, :start_time, allow_nil: true
     
     belongs_to :user
 
