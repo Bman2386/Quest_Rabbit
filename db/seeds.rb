ab2ea4aed6e5bd9ea68a84531fd2159451fbfee5
Brendon <brendon.biagi@gmail.com>
2020-09-28 14:16:55 -0400

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.delete_all
+# User.delete_all
+
+Category.create({
+    id: 1,
+    category_name: 'Fetch',
+    ex_description: 'Find and deliver 13 pink frost crystals'
+})
+
+Category.create({
+    id: 2,
+    category_name: 'Craft',
+    ex_description: 'Craft an alchemy table'
+})
+
+Category.create({
+    id: 3,
+    category_name: 'Escort',
+    ex_description: 'Escort the princess through the forbidden forest'
+})
+
+Category.create({
+    id: 4,
+    category_name: 'Slay',
+    ex_description: 'Slay that pesky dragon terrorizing the village'
+})
+
+# User.create({})
