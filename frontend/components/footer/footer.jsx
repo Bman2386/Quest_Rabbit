1dd77df98702ff1aa103d9e06ed831887fc136c5
Brendon <brendon.biagi@gmail.com>
2020-12-18 12:16:47 -0500

    fixed links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -14,9 +14,9 @@ class Footer extends React.Component {
         const git = window.git
         return (
             <div className="footer">
-                <Link to="https://www.linkedin.com/in/brendonbiagi/" className='a'><img src={linked} className="linked"/></Link>
-                <Link to="https://github.com/Bman2386" className='a'><img src={git} className="linked"/></Link>
-                <Link to="https://bman2386.github.io/Brendon.Biagi/" className="creator">By Brendon Biagi</Link>
+                <a target="_blank" href="https://www.linkedin.com/in/brendonbiagi/" className='a'><img src={linked} className="linked"/></a>
+                <a target="_blank" href="https://github.com/Bman2386" className='a'><img src={git} className="linked"/></a>
+                <a target="_blank" href="https://bman2386.github.io/Brendon.Biagi/" className="creator">By Brendon Biagi</a>
             </div>
         )
     }
