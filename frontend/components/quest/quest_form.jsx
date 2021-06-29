affd52c9238eeb484a70048cb871c5c2709476c5
Brendon <brendon.biagi@gmail.com>
2021-06-28 20:09:03 -0400

    remove unnecissary comments

--- frontend/components/quest/quest_form.jsx
+++ frontend/components/quest/quest_form.jsx
@@ -370,74 +370,10 @@ class QuestForm extends React.Component {
               )
           }
         }
-        // render(){
-        //     return(
-        //         <div>
-        //             {this.renderForm()}
-        //         </div>
-        //     )
-        // }
+        
     }
 
 export default QuestForm;
     
-
-    // renderForm2(){
-        
-    //     return(
-    //         <div>
-    //             <ul className='bar1'>
-    //             <li className='currentN'>1</li>
-    //             <div className='lineN'></div>
-    //             <li className='grey-out'>2</li>
-    //             <div className='lineN'></div>
-    //             <li className='grey-out'>3</li>
-    //         </ul>
-    //         <ul>
-    //             <li className='current'>Describe your Quest</li>
-    //             <li className='line'>Browse Adventurers</li>
-    //             <li className='line'>Choose date {'&'} Time</li>
-    //         </ul>
-    //         <form onSubmit={this.handleSubmit} className="quest-form">
-    //         <div>{this.list()}</div>
-    //         </form>
-    //         </div>
-    //     )
-    // }
-
-    // renderForm3(){
-    //     return(
-    //         <div>
-    //             <ul className='bar1'>
-    //             <li className='currentN'>1</li>
-    //             <div className='lineN'></div>
-    //             <li className='grey-out'>2</li>
-    //             <div className='lineN'></div>
-    //             <li className='grey-out'>3</li>
-    //         </ul>
-    //         <ul>
-    //             <li className='current'>Describe your Quest</li>
-    //             <li className='line'>Browse Adventurers</li>
-    //             <li className='line'>Choose date {'&'} Time</li>
-    //         </ul>
-    //         <p>choose date format</p>
-    //         </div>
-    //     )
-    // }
-    // formChanger(){
-    //         if (this.status === 1){
-    //             return this.renderForm1()
-    //         } else if (this.status === 2){
-    //             return this.renderForm2()
-    //         } else if (this.status === 3){
-    //             return this.renderForm3()
-    //         }
-    //     }
-
-    // render(){
-    //     return (
-    //     <div>{this.formChanger()}</div>
-    //     )
-    // }
 
 
