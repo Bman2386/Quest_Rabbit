9df2d9a40becb67b035a628f1b3bce75c0a9c8c4
Brendon <brendon.biagi@gmail.com>
2021-08-03 19:15:03 -0400

    remove extra comments

--- frontend/components/quest/quest_page_container.jsx
+++ frontend/components/quest/quest_page_container.jsx
@@ -3,8 +3,7 @@ import QuestPage from './quest_page';
 import { fetchQuests} from '../../actions/quest';
 import {fetchAdventurers} from '../../actions/adventurer'
 
-const mSTP = (state, ownProps) => {
-    // const quests = state.entities.quests
+const mSTP = (state) => {
     const creatorId = state.session.currentUser.id;
     const adv = Object.keys(state.entities.adventurers).map(key => state.entities.adventurers[key])
     return ({
@@ -17,8 +16,6 @@ const mSTP = (state, ownProps) => {
 const mDTP = dispatch => ({
     getQuests: (creatorId) => dispatch(fetchQuests(creatorId)),
     fetchAdv: () => dispatch(fetchAdventurers())
-    // getQuest: questId => dispatch(fetchQuest(questId)),
-    // updateQuest: questId => dispatch(updateQuest(questId))
 })
 
 export default connect(mSTP, mDTP)(QuestPage);
