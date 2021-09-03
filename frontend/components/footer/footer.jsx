37e651eefbab513f1027a5b45aea742dd8a75ab5
Brendon <brendon.biagi@gmail.com>
2021-09-03 16:32:38 -0400

    add hover effects to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 class Footer extends React.Component {
 
-    constructor(props) {
-        super(props)
+    // constructor(props) {
+    //     super(props)
         
-    }
+    // }
 
     render(){
         const linked = window.linked;
@@ -13,7 +13,7 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <a target="_blank" href="https://www.linkedin.com/in/brendonbiagi/" className='a'><img src={linked} className="linked"/></a>
-                <a target="_blank" href="https://github.com/Bman2386" className='a'><img src={git} className="linked"/></a>
+                <a target="_blank" href="https://github.com/Bman2386" className='a'><img src={git} className="linked2"/></a>
                 <a target="_blank" href="https://bman2386.github.io/Brendon.Biagi/" className="creator">By Brendon Biagi</a>
             </div>
         )
